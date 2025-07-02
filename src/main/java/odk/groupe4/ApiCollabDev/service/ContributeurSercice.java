57a33f506b519c037d97eeb235cddc1fa7396228
madyeh sylla <madyehsylla427@gmail.com>
2025-07-02 09:12:18 +0200

    ÃCreation Api Contributeur

--- src/main/java/odk/groupe4/ApiCollabDev/service/ContributeurSercice.java
+++ src/main/java/odk/groupe4/ApiCollabDev/service/ContributeurSercice.java
@@ -2,6 +2,7 @@ package odk.groupe4.ApiCollabDev.service;
 
 import odk.groupe4.ApiCollabDev.dao.ContributeurDao;
 import odk.groupe4.ApiCollabDev.dto.ContributeurDto;
+import odk.groupe4.ApiCollabDev.models.Contributeur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,17 @@ public class ContributeurSercice {
     @Autowired
     private ContributeurDao contributeurDao;
 
-    public void ajouterContributeur(ContributeurDto c) {
+    public Contributeur ajouterContributeur(ContributeurDto contributeur) {
 
-        // Instanciation d'un objet Contributeur
+        Contributeur contrib =new Contributeur();
         // Conversion de l'objet ContributeurDAO en Contributeur
+        contrib.setNom(contributeur.getNom());
+        contrib.setPrenom(contributeur.getPrenom());
+        contrib.setTelephone(contributeur.getTelephone());
+        contrib.setEmail(contributeur.getEmail());
+        contrib.setPassword(contributeur.getPassword());
+        contrib.setTotalCoin(contributeur.getTotalCoin());
         // Enregistrement de l'objet Contributeur
+        return contributeurDao.save(contrib);
     }
 }
--- src/main/java/odk/groupe4/ApiCollabDev/service/ContributionService.java
+++ src/main/java/odk/groupe4/ApiCollabDev/service/ContributionService.java
@@ -6,6 +6,5 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class ContributionService {
 }
