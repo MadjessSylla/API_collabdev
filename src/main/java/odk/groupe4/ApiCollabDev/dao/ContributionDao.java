cf3c893166ba99188ca66781e1092ab86be9916b
Aboubacarsogoba <aboubacarsogoba@gmail.com>
2025-07-04 16:40:32 +0000

    quelques fonctionnalité ajouter

--- src/main/java/odk/groupe4/ApiCollabDev/dao/ContributionDao.java
+++ src/main/java/odk/groupe4/ApiCollabDev/dao/ContributionDao.java
@@ -2,13 +2,15 @@ package odk.groupe4.ApiCollabDev.dao;
 
 import odk.groupe4.ApiCollabDev.models.Contribution;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ContributionDao extends JpaRepository<Contribution, Integer> {
-    // Methode qui retourne la liste des contributions d'un utilisateur
-    List<Contribution> findByUserId(int userId);
+    @Query("SELECT c FROM Contribution c WHERE c.participant.contributeur.id = :idUtilisateur")
+    List<Contribution> findByUserId(@Param("idUtilisateur") Integer idUtilisateur);
 }
