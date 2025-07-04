2c9dcc4aaee083a7af53493ccebf9406d4268d66
Djibril Maïga <maigadjibril444@gmail.com>
2025-07-04 20:58:54 +0000

    Mise à jour de l'api pour les badges

--- src/main/java/odk/groupe4/ApiCollabDev/controllers/BadgeController.java
+++ src/main/java/odk/groupe4/ApiCollabDev/controllers/BadgeController.java
@@ -1,10 +1,13 @@
 package odk.groupe4.ApiCollabDev.controllers;
 
+import odk.groupe4.ApiCollabDev.dto.BadgeCoinDescDto;
 import odk.groupe4.ApiCollabDev.dto.BadgeDto;
 import odk.groupe4.ApiCollabDev.models.Badge;
 import odk.groupe4.ApiCollabDev.service.BadgeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/badges")
@@ -29,10 +32,26 @@ public class BadgeController {
         return badgeService.obtenirBadgeParId(idBadge);
     }
     // GET All
+    @GetMapping
+    public List<Badge> obtenirTousLesBadges(){
+        return badgeService.obtenirTousLesBadges();
+    }
 
     // PUT
+    @PutMapping("/{idBadge}")
+    public Badge miseAJourBadge(@PathVariable("{idBadge}") int idBadge,@RequestBody BadgeDto badgeDto){
+        return badgeService.mettreAJourBagde(idBadge, badgeDto);
+    }
 
     // PATCH
+    @PatchMapping("{id}")
+    public Badge miseAJourCoinDesc(@PathVariable("{id}") int idBadge,@RequestBody BadgeCoinDescDto dto){
+        return badgeService.mettreAJourCoinEtDescription(idBadge, dto);
+    }
 
     // DELETE
+    @DeleteMapping("{id}")
+    public String supprimerBadge(@PathVariable("{id}") int idBadge){
+        return badgeService.supprimerBadge(idBadge);
+    }
 }
